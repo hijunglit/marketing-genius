563b0c1606c60a6768d16a70df233a96ae18b451
hijunglit <hijunglit@gmail.com>
2026-03-13 11:14:18 +0900

    fixed logoUrl

--- app/common/home.tsx
+++ app/common/home.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export default function Home() {
   const logoUrl =
-    "https://github-production-user-asset-6210df.s3.amazonaws.com/113867021/562087579-a66c730e-e08c-434f-bb8a-ff57e7209f59.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20260312%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260312T082743Z&X-Amz-Expires=300&X-Amz-Signature=e93be753ddb6144f7e15452ff756515cc2e6ba5e5bed915512ab93dc5f08f901&X-Amz-SignedHeaders=host";
+    "https://github-production-user-asset-6210df.s3.amazonaws.com/113867021/562674819-a236fc1e-039f-445b-b4a4-6e53ed473d4c.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20260313%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260313T021342Z&X-Amz-Expires=300&X-Amz-Signature=112392c0854ef066cb1d18b53399658dfc70b1762166c4ff417823c6034865e6&X-Amz-SignedHeaders=host";
   const logoText =
     "https://github-production-user-asset-6210df.s3.amazonaws.com/113867021/562080151-8d33a8df-2673-4c2f-96eb-8d684abe3821.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20260312%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260312T073413Z&X-Amz-Expires=300&X-Amz-Signature=3b77ca4a0d0094b83a4027cb1bf9e05e93cf6512360d10145e553950d09c5fc5&X-Amz-SignedHeaders=host";
   return (
